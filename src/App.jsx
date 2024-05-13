23483fd1502ceb1e1d7f51d6f6b050a8c4d88316
eemenalop <eemena26@gmail.com>
2024-05-13 16:37:48 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Header } from "./componets/header"
+
+function App() {
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ src/componets/header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+      <header className="bg-red-500">
+          <h1>Esto es el header</h1>
+    </header>
+  )
+}
